4a169939a7ca36db48a5faf7940c05f7c9fa1f26
Nicolás Oses <127158636+nicoalexanderdev@users.noreply.github.com>
2025-05-06 16:06:59 -0400

    add CustomButton

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -15,7 +15,7 @@ const CustomButton: React.FC<CustomButtonProps> = ({ title, handlePress, contain
         onPress={handlePress}
         activeOpacity={0.7}
 
-        className={`bg-secondary rounded-xl min-h-[62px] justify-center items-center 
+        className={`bg-secondary-200 rounded-xl min-h-[62px] justify-center items-center 
             ${containerStyles}
             ${isLoading ? 'opacity-50' : ''}`}
             disabled={isLoading}
